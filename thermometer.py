bf06460c1ed202378cf5351edbfa5125591573fe
Mirco Mannino <45531787+mircomannino@users.noreply.github.com>
2019-08-26 16:34:15 +0200

    Update thermometer.py

--- thermometer.py
+++ thermometer.py
@@ -5,29 +5,30 @@ import time
 # the servo motor.
 
 def main():
-    # Reading data from the MPL3115 sensor
-    TEMP_RAW = ""
-    with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_raw", "r") as raw_file:
-        TEMP_RAW = raw_file.read()
-    TEMP_SCALE = ""
-    with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_scale", "r") as scale_file:
-        TEMP_SCALE = scale_file.read()
+    while(True):
+        # Reading data from the MPL3115 sensor
+        TEMP_RAW = ""
+        with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_raw", "r") as raw_file:
+            TEMP_RAW = raw_file.read()
+        TEMP_SCALE = ""
+        with open("/sys/class/i2c-dev/i2c-1/device/1-0060/iio:device0/in_temp_scale", "r") as scale_file:
+            TEMP_SCALE = scale_file.read()
 
-    # Open the serial communication
-    ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
-    ser.flushOutput()
+        # Open the serial communication
+        ser = serial.Serial("/dev/ttyS0", 115200, timeout=1)
+        ser.flushOutput()
 
-    # mapping for temperatures
-    map_temp = {25: 150, 26: 120, 27: 90, 28: 60, 29: 30}
+        # mapping for temperatures
+        map_temp = {25: 150, 26: 120, 27: 90, 28: 60, 29: 30}
 
-    # Compute temperature and send it to the servo motor
-    temp = int(TEMP_RAW) * int(TEMP_SCALE)
-    degrees_to_servo = map_temp[int(temp)]
-    print("Temperatura: " + str(int(temp)) + "* ")
-    print("Degrees servo: " + str(degrees_to_servo))
-    ser.write(chr(degrees_to_servo))
+        # Compute temperature and send it to the servo motor
+        temp = float(TEMP_RAW) * float(TEMP_SCALE)
+        degrees_to_servo = map_temp[int(temp)]
+        print("Temperatura: " + str(int(temp)) + "* ")
+        print("Degrees servo: " + str(degrees_to_servo))
+        ser.write(chr(degrees_to_servo))
     
-    time.sleep(3)
+        time.sleep(3)
 
 if __name__ == '__main__':
     main()
